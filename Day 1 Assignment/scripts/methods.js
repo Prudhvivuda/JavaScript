a4dcfb88cfc96500d8730debe7be55357e22e42a
Prudhvi Vuda <53619858+Prudhvivuda@users.noreply.github.com>
2020-07-14 23:10:10 +0530

    Add files via upload

---
+++ Day 1 Assignment/scripts/methods.js
@@ -0,0 +1,15 @@
+console.warn('U r going to explore console methods!!'); 
+
+console.time('abc'); 
+
+var str = "console.log() method"; 
+
+console.log(str); 
+
+console.error('This is console.error() method');  
+
+console.table("This is console.table() method"+{name:'prudhvi', age:'19'}); 
+
+console.timeEnd('abc'); 
+
+//console.clear();  
