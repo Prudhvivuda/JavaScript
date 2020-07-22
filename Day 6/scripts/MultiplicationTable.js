3ffb7d3cf34a0b70834c814fe2cc4a5a98dbf5b5
Prudhvi Vuda <53619858+Prudhvivuda@users.noreply.github.com>
2020-07-22 15:28:17 +0530

    Add files via upload

---
+++ Day 6/scripts/MultiplicationTable.js
@@ -0,0 +1,8 @@
+function printTable(){
+    var n;
+    n=Number(document.getElementById('txtNumber').value);
+    for(let i=1;i<=20;i++)
+    {
+        document.write(`${n} X  ${i} =  ${n*i} <br/>`);
+    }
+}    
